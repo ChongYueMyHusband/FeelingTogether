ea31dd6f336455903e9b3ba3a04669949dd27a19
WuGuohuaNo1 <133508861+WuGuohuaNo1@users.noreply.github.com>
2023-12-15 17:04:19 +0800

    Add files via upload

---
+++ Back_end/src/main/java/com/example/project_ee297/Object/Diary.java
@@ -0,0 +1,13 @@
+package com.example.project_ee297.Object;
+
+public class Diary {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
